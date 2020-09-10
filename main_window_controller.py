8b09420a7d1c6c299474c9439332127129bcfb90
Caleb <cmrichardson1993@gmail.com>
2020-09-09 17:27:46 -0700

    added load from file and looping photos
    MainWIndowController now loads photos from the photo dir into a list and the next button can loop through them.

--- main_window_controller.py
+++ main_window_controller.py
@@ -1,22 +1,41 @@
 from controller_base import ControllerBase
 from main_window import MainWindow
 from picture import Picture
+import os
+import settings
 
 class MainWindowController(ControllerBase):
 
     def __init__(self, app):
         super().__init__(app)
 
+        self.pictures = []
+        self.index = 0
+        
         self.init_controller()
 
     def init_controller(self):
         self._view = MainWindow()
+        self.load()
         self._view.next_button.clicked.connect(self.next_button_pressed)
-        self._view.update_picture(Picture('photo1.jpg'))
+        if len(self.pictures) > 0:
+            self._view.update_picture(self.pictures[self.index])
 
     def next_button_pressed(self):
-       self._view.update_picture(Picture('photo2.jpg'))
+        if (self.index) == len(self.pictures) -1:
+            self.index = 0
+        else:
+            self.index+=1
+        self._view.update_picture(self.pictures[self.index])
         
     def run(self):
         self._view.show()
         return self._app.exec_()
+
+    # load photos from photos directory
+    def load(self):
+
+        for file in os.listdir(settings.PHOTO_DIR):
+            p = Picture(os.path.join(settings.PHOTO_DIR, file))
+            self.pictures.append(p)
+        
--- picture.py
+++ picture.py
@@ -3,21 +3,19 @@ import os
 
 class Picture():
 
-    def __init__(self, _file_name):
+    def __init__(self, file_path):
         super().__init__()
 
         self.error = False
-        self.path_to_file = ''
+        self.path_to_file = file_path
         
-        self.init_photo(_file_name)
+        self.init_photo()
 
         if self.error == True:
             self.init_photo(settings.BROKEN_LINK)
             print('Picture Error: Broken Link')
 
-    def init_photo(self, file_name):
-        
-        self.path_to_file = os.path.join(settings.PHOTO_DIR, file_name)
+    def init_photo(self):
 
         if os.path.exists(self.path_to_file) != True:
             self.error = True
